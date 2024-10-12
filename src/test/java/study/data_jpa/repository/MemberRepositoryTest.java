f73575dc28f23675e5b396be00f6ed8ad2015ae5
aammddkkzxc <ideal0427@naver.com>
2024-10-12 20:37:25 +0900

    feat: @Query, 리포지토리 메소드에 쿼리 정의하기 - @Query는 이름이 없는 네임드 쿼리라고 볼 수 있음 (컴파일 할 떄 문법오류 잡아줌) - 조건이 복잡해지면 이름으로 쿼리를 만들어 주는 기능을 사용하면 메서드 명이 너무 길어진다. - 그냥 복잡할 때도 사용

--- src/test/java/study/data_jpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/data_jpa/repository/MemberRepositoryTest.java
@@ -105,4 +105,15 @@ class MemberRepositoryTest {
         System.out.println(result.get(1).getAge());
     }
 
+    @Test
+    public void testQuery() {
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("AAA", 20);
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<Member> result = memberRepository.findUser("AAA", 10);
+        assertThat(result.get(0)).isEqualTo(m1);
+    }
+
 }
