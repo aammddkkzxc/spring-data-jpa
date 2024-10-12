76afd62a13116dfb7a818de1a949b0f01a41c46d
aammddkkzxc <ideal0427@naver.com>
2024-10-12 19:02:21 +0900

    feat: 순수 JPA 기반 리포지토리 만들기

---
+++ src/main/java/study/data_jpa/repository/TeamRepository.java
@@ -0,0 +1,42 @@
+package study.data_jpa.repository;
+
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import study.data_jpa.entity.Team;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+@RequiredArgsConstructor
+public class TeamRepository {
+
+    private final EntityManager em;
+
+    public Team save(Team team) {
+        em.persist(team);
+        return team;
+    }
+
+    public void delete(Team team) {
+        em.remove(team);
+    }
+
+    public Team find(Long id) {
+        return em.find(Team.class, id);
+    }
+
+    public Optional<Team> findById(Long id) {
+        return Optional.ofNullable(em.find(Team.class, id));
+    }
+
+    public List<Team> findAll() {
+        return em.createQuery("select t from Team t", Team.class).getResultList();
+    }
+
+    public long count() {
+        return em.createQuery("select count(*) from Team", Long.class).getSingleResult();
+    }
+
+}
