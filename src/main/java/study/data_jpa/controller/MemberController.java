25c4839f512e6db9a06b5ecc00d1ed57a2e0c4e5
aammddkkzxc <ideal0427@naver.com>
2024-10-13 22:14:47 +0900

    feat: Web 확장 - 페이징과 정렬 - 스프링 부트가 PageRequest구현체를 알아서 세팅해서 Pageable 파라미터에 넣어줌, 요청 파라미터로 page,size,sort다 가능 - 기본 설정 바꾸려면 글로벌 설정, @PageableDefault사용 - 페이징 정보가 둘 이상일 경우 @Qualifier사용 - 페이지를 1부터 시작하려면   - 직접 구현체를 만들거나   - spring.data.web.pageabl.one-indexed-parameters 를 true 로 설정 => 이 방법은 api로 넘어가는 page상세 스펙내용에는 반영이 안됨

--- src/main/java/study/data_jpa/controller/MemberController.java
+++ src/main/java/study/data_jpa/controller/MemberController.java
@@ -2,9 +2,14 @@ package study.data_jpa.controller;
 
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import study.data_jpa.dto.MemberDto;
 import study.data_jpa.entity.Member;
 import study.data_jpa.repository.MemberRepository;
 
@@ -25,9 +30,24 @@ public class MemberController {
         return member.getUsername();
     }
 
+    @GetMapping("/members")
+    public Page<Member> findMembers(Pageable pageable) {
+        return memberRepository.findAll(pageable);
+    }
+
+    @GetMapping("/members2")
+    public Page<MemberDto> findMembers2(@PageableDefault(size = 5, sort = "username", direction = Sort.Direction.DESC) Pageable pageable) {
+        Page<Member> page = memberRepository.findAll(pageable);
+        Page<MemberDto> map = page.map(member -> new MemberDto(member.getId(), member.getUsername(), null));
+
+        return map;
+    }
+
     @PostConstruct
     public void init() {
-        memberRepository.save(new Member("mem1"));
+        for (int i = 0; i < 100; i++) {
+            memberRepository.save(new Member("member" + i, i));
+        }
     }
 
 }
