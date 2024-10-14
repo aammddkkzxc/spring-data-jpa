b78feba545dff84cc9097538dc4fc619021574f0
aammddkkzxc <ideal0427@naver.com>
2024-10-14 18:54:24 +0900

    feat: 네이티브 쿼리 - 가급적 네이티브 쿼리는 사용하지 않는게 좋음, 정말 어쩔 수 없을 때 사용 - 정적 네이티브 쿼리의 경우 스프링 데이터 JPA 네이티브 쿼리 + 인터페이스 기반 Projections 활용해서 풀기 가능 - 동적 네이티브 쿼리의 경우 jdbctemplate 등 사용

--- src/test/java/study/data_jpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/data_jpa/repository/MemberRepositoryTest.java
@@ -338,4 +338,39 @@ class MemberRepositoryTest {
             System.out.println("usernameOnly: " + projection);
         }
     }
+
+    @Test
+    public void nativeQuery() {
+        Team teamA = new Team("teamA");
+        em.persist(teamA);
+        Member m1 = new Member("m1", 0, teamA);
+        Member m2 = new Member("m2", 0, teamA);
+        em.persist(m1);
+        em.persist(m2);
+        em.flush();
+        em.clear();
+
+        Member result = memberRepository.findNativeByQuery("m1");
+        System.out.println("result = " + result);
+    }
+
+    @Test
+    public void nativeQueryProjection() {
+        Team teamA = new Team("teamA");
+        em.persist(teamA);
+        Member m1 = new Member("m1", 0, teamA);
+        Member m2 = new Member("m2", 0, teamA);
+        em.persist(m1);
+        em.persist(m2);
+        em.flush();
+        em.clear();
+
+        Page<MemberProjection> result = memberRepository.findByNativeProjection(PageRequest.of(0, 10));
+        List<MemberProjection> content = result.getContent();
+        for (MemberProjection memberProjection : content) {
+            System.out.println("username = " + memberProjection.getUsername());
+            System.out.println("teamName = " + memberProjection.getTeamName());
+        }
+        System.out.println("result = " + result);
+    }
 }
